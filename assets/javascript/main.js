a74cbf3fb1566b09e8d71a915f2002743fc59020
HOÀNG ĐỨC VINH <20200120@gmail.com>
2021-10-13 14:59:53 +0700

    fix slide va position section

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -26,27 +26,30 @@ document.querySelectorAll(".image-slide img").forEach(images => {
         document.querySelector('.home-img').src = src
     };
 })
+
 var swiper = new Swiper(".review-slider", {
+    slidesPerView: 1,
     spaceBetween: 20,
+    loop: true,
+    grabCursor: true,// hinh ban tay
+    centeredSlides: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
-    autoplay: {
-        delay: 7500,
-        disableOnInteraction: false,
-    },
     breakpoints: {
         0: {
-            slidesPerView: 0,
-
+            slidesPerView: 1
         },
         768: {
-            slidesPerView: 2,
-
+            slidesPerView: 2
         },
-        loop: true,
-        grabCursor: true,
-
+        991: {
+            slidesPerView: 3
+        },
     },
 });
